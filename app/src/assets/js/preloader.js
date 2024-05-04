80f32a6d3221c6650543e35dc5125b356d88c029
lakubuDavid <Lakubudavid@gmail.com>
2024-05-04 20:10:55 +0400

    A lot of fixes and some pages on the reservation part

--- app/src/assets/js/preloader.js
+++ app/src/assets/js/preloader.js
@@ -5,19 +5,20 @@ function fadeOutEffect(target) {
             fadeTarget.style.opacity = 1;
         }
         if (fadeTarget.style.opacity > 0) {
-            fadeTarget.style.opacity -= 0.05;
+            fadeTarget.style.opacity -= 0.1;
         } else {
             clearInterval(fadeEffect);
         }
     }, 50);
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    // console.log("startted")
-    const loader = document.querySelector("#preloader")
-    // fadeOutEffect(loader)
-    setTimeout(() => {
-        fadeOutEffect(loader)
-        // console.log("done")
-    }, 200);
-});
+    // loader.style.opacity = 1
+    document.addEventListener("DOMContentLoaded", function (event) {
+        // console.log("startted")
+        const loader = document.querySelector("#preloader")
+        // fadeOutEffect(loader)
+        setTimeout(() => {
+            fadeOutEffect(loader)
+            // console.log("done")
+        }, 200);
+    });
