d04f65ee6f82e9711caf861df54959938f021e5b
lakubuDavid <Lakubudavid@gmail.com>
2024-05-04 15:04:04 +0400

    Services page addded and small fixed

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages";
 
 import nunjucks from "nunjucks";
 import { nunjucksRouter } from "./utils";
+import { server } from "./server";
 
 const router = new Hono();
 const nunExcludedPaths = ["_components"];
@@ -29,6 +30,28 @@ router.get("/", (c) => {
   }
 });
 
+
+router.get("/reserve", (c) => {
+  const nun = new nunjucks.Environment(
+    new nunjucks.FileSystemLoader(process.env.NODE_ENV == "production" ? "views": "src/views"),
+    {
+      autoescape: true,
+      trimBlocks: true,
+      watch: true,
+    }
+  );
+
+  const temp = nun.getTemplate("reserve/index.html");
+
+  try {
+    let h = temp.render({});
+    // console.log(h);
+    return c.html(h);
+  } catch (e) {
+    return c.notFound();
+  }
+});
+
 router.get("/*", (c) => {
   const nun = new nunjucks.Environment(
     new nunjucks.FileSystemLoader(process.env.NODE_ENV == "production" ? "views": "src/views"),
@@ -42,5 +65,7 @@ router.get("/*", (c) => {
     query:c.req.query()
   });
 });
+
+router.route("/",server);
 
 export { router };
